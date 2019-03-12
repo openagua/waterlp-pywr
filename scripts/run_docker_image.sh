9583ef6f189efd0aa396bb13099e83e067eb8fe8
david <drheinheimer@umass.edu>
2019-03-12 19:41:01 -0400

    misc updates

--- scripts/run_docker_image.sh
+++ scripts/run_docker_image.sh
@@ -2,4 +2,4 @@
 docker run --name redis -p 6379:6379 -d redis
 docker pull openagua/waterlp-pywr:celery
 docker rm --force waterlp
-docker run -d --env-file ./instance/env.list --link redis:redis --volume /home/ubuntu:/home/root --volume /etc/localtime:/etc/localtime  --name waterlp openagua/waterlp-pywr:celery
+docker run -d --env-file ./instance/variables.env --link redis:redis --volume /home/ubuntu:/home/root --volume /etc/localtime:/etc/localtime  --name waterlp openagua/waterlp-pywr:latest
