23d6b7e11858ca369dc09148b167bc0ca3c024f4
david <drheinheimer@umass.edu>
2019-03-22 18:21:32 -0400

    update docker compose bash script

--- scripts/run_docker_compose.sh
+++ scripts/run_docker_compose.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-apt-get update -y && apt-get install -y docker-compose
+apt-get install -y docker-compose
 wget https://github.com/openagua/waterlp-pywr/raw/daily/docker-compose.yml
 docker-compose up
