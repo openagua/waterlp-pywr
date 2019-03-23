dc131590ebef0b12777d36b6ebf0d160ef25083f
david <drheinheimer@umass.edu>
2019-03-23 18:30:55 -0400

    update docker compose yml

--- scripts/run_docker_compose.sh
+++ scripts/run_docker_compose.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 apt-get install -y docker-compose
-wget https://github.com/openagua/waterlp-pywr/raw/daily/docker-compose.yml
+docker pull openagua/waterlp-pywr:sanjoaquin
+docker rm --force waterlp
+wget https://github.com/openagua/waterlp-pywr/raw/sanjoaquin/docker-compose.yml
 docker-compose up
--- scripts/run_docker_image.sh
+++ scripts/run_docker_image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker run --name redis -p 6379:6379 -d redis
-docker pull openagua/waterlp-pywr:celery
+docker pull openagua/waterlp-pywr:sanjoaquin
 docker rm --force waterlp
-docker run -d --env-file ./instance/variables.env --link redis:redis --volume /home/ubuntu:/home/root --volume /etc/localtime:/etc/localtime  --name waterlp openagua/waterlp-pywr:latest
+docker run -d --env-file ./variables.env --link redis:redis --volume /home/ubuntu:/home/root --volume /etc/localtime:/etc/localtime  --name waterlp openagua/waterlp-pywr:sanjoaquin
